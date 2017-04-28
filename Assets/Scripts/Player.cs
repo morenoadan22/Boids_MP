12869ac4b6308fd40a29650a8c7313805b26d793
morenoadan22 <morenoadan22@gmail.com>
2017-04-27 23:45:19 -0700

    add player object

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+	private Transform cannon;
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+
+	void Awake(){
+		cannon = this.gameObject.transform.GetChild (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		cannon.transform.Rotate (y, 0, -x);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
+		}
+	}
+
+	[Command]
+	void CmdFire(){
+		GameObject bullet = Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 200;
+
+		NetworkServer.Spawn (bullet);
+
+		Destroy (bullet, 2);
+	}
+
+	public override void OnStartLocalPlayer ()
+	{
+		base.OnStartLocalPlayer ();
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+}
